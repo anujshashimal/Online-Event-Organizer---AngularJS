4dd3d58bf24899e382f395eb4c582524bf0a8004
unknown <anujshashimal456@gmail.com>
2020-04-10 11:34:23 +0530

    Event crud operation done

--- app/app.js
+++ app/app.js
@@ -1,32 +1,41 @@
 'use strict';
 
-// Declare app level module which depends on views, and core components
-// angular.module('myApp', [
-//   'ngRoute',
-//   'myApp.view1',
-//   'myApp.view2',
-//   'myApp.version'
-// ]).
-// config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-//   $locationProvider.hashPrefix('!');
-//
-//   $routeProvider.otherwise({redirectTo: '/view1'});
-// }]);
-
 var myApp = angular.module("myApp", []);
 myApp.controller("myController", function ($scope) {
     console.log("Controller");
 
     $scope.newEvent = {};
+    $scope.clickedEvent = {};
 
     $scope.events = [
-        // {eventDescitption : "anuj", DateNTime: "12/12/12"}
+        {eventDescitption : "anuj", DateNTime: "12/12/12"}
     ];
 
     $scope.saveEvent = function () {
     console.log('Event Saved!')
-        console.log($scope.newEvent);
         $scope.events.push($scope.newEvent);
+        $scope.newEvent = {};
+        $scope.EventMsg = "New Event Added!";
+
+    }
+
+    $scope.selectEvent = function (event) {
+        console.log(event)
+        $scope.clickedEvent = event
+    }
+
+    $scope.updateEvent = function () {
+        $scope.EventMsg = "Event Updated Successfually!"
+
+    }
+
+    $scope.clearMsg = function(){
+        $scope.EventMsg = ""
+    }
+
+    $scope.deleteEvent = function () {
+        $scope.events.splice($scope.events.indexOf($scope.clickedEvent), 1);
+        $scope.EventMsg = "Event Successfually Deleted!"
 
     }
 
